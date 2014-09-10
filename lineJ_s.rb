f7e708c2ccdae0bcdc43cb4b7018c208fcdd7dc7
Sirius Wang <larrywhy.tw@gmail.com>
2014-09-11 00:14:59 +0800

    test

--- lineJ_s.rb
+++ lineJ_s.rb
@@ -3,9 +3,14 @@ require 'em-websocket'
 puts "Server is listening!..."
 EM.run {
   EM::WebSocket.run(:host => "0.0.0.0", :port => 8080) do |ws|
+
+    #puts ws.methods.sort
+
+    puts ws.onopen.methods.sort
     ws.onopen { |handshake|
       puts "WebSocket connection open"
-      puts handshake.path
+      #puts "[" + handshake.path + "]"
+      #puts handshake.methods.sort
 
       # Access properties on the EM::WebSocket::Handshake object, e.g.
       # path, query_string, origin, headers
@@ -16,11 +21,11 @@ EM.run {
     }
 
     ws.onclose { puts "Connection closed" }
-
+    #puts ws.onmessage.methods.sort
     ws.onmessage { |msg|
       puts "Recieved message: #{msg}"
       ws.send "s: #{msg} ."
-      ws.send "s: Hello , welcome to Line_J"
+      #ws.send "s: Hello , welcome to Line_J"
     }
   end
 }
