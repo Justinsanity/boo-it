89d1fbf25a4bb2dd8e40424190202dd81546444e
Sirius Wang <larrywhy.tw@gmail.com>
2014-09-13 01:09:01 +0800

    new update , and have written client finish, but still not encrypt and not complete . Have to make an authentication server

--- lineJ_s.rb
+++ lineJ_s.rb
@@ -1,46 +1,50 @@
 require 'em-websocket'
 
 puts "Server is listening!..."
+@uid = []
+@sid = []
+@channel_list = []
 EM.run {
     @channel = EM::Channel.new
-    #puts @channel.class
-    #puts @channel.methods.sort
+    #puts @channel.methods
   EM::WebSocket.run(:host => "0.0.0.0", :port => 8080) do |ws|
 
     #puts ws.methods.sort
 
     #puts ws.onopen.methods.sort
     ws.onopen { |handshake|
+      #@channel = EM::Channel.new
       puts "WebSocket connection open"
-      #puts "[" + handshake.path + "]"
-      #puts handshake.query
-      #puts handshake.origin
-      #puts handshake.methods.sort
-      sid = @channel.subscribe {|msg|
-          
+      # first message to subscribe channel
+      @sid << @channel.subscribe {|msg|
+          #puts "XXX"
+          puts "received:" + msg + "---"
           ws.send msg # send itself
       }
-      puts sid
+      tid = @sid.last
       # broadcast each connection users
-      @channel.push "#{sid} connect!"
-
+      @channel.push "#{@sid.last} connect!"
       ws.onmessage {|msg|
-          @channel.push "<#{sid}>: #{msg}"
-          
+        @uid = msg.split(/,/)
+	#ws.send "---" + @uid[0] + "---"
+        # Here are tickets
+	#if @uid[0] ==  "larry" && @uid[1] == "veck" && tid == 1
+
+           #@channel.push "<#{tid}>: #{msg}"
+           @channel.push "#{msg}"
+
+           #puts @channel.subs.to_s
+	#end
+        #ws.send msg
+      #puts @uid[0]
       }
 
       ws.onclose {
-          @channel.unsubscribe(sid)
+          @channel.unsubscribe(tid)
+          @channel.push "#{tid} conection closed!"
       }
       
-
-      # Access properties on the EM::WebSocket::Handshake object, e.g.
-      # path, query_string, origin, headers
-
-      # Publish message to the client
-
-      #ws.send "Hello Client, you connected to #{handshake.path}"
-    }
+    }  # onopen end
 
     #ws.onclose { puts "Connection closed" }
     #puts ws.onmessage.methods.sort
@@ -48,6 +52,5 @@ EM.run {
     #  puts "Recieved message: #{msg}"
     #  ws.send "s: #{msg} ."
       #ws.send "s: Hello , welcome to Line_J"
-    #}
   end
 }
--- s_lienJ_s.rb
+++ s_lienJ_s.rb
@@ -1,23 +1,32 @@
 require 'em-websocket'
-
+@sid
 puts "Server is listening!..."
 EM.run {
+  @channel = EM::Channel.new
   EM::WebSocket.run(:host => "0.0.0.0", :port => 8080) do |ws|
     ws.onopen { |handshake|
       puts "WebSocket connection open"
+      @sid = @channel.subscribe {|msg|
+          puts "---" + msg + "---"
+          ws.send msg # send itself
+      }
+      puts @sid.to_s + " connected!"
       # path, query_string, origin, headers
-
       # Publish message to the client
-
       #ws.send "Hello Client, you connected to #{handshake.path}"
     }
+    
 
-    ws.onclose { puts "Connection closed" }
+    ws.onclose { 
+	puts @sid.to_s + "Connection closed" 
+	@channel.unsubscribe(@sid)
+    }
     #puts ws.onmessage.methods.sort
     ws.onmessage { |msg|
       puts "Recieved message: #{msg}"
-      ws.send "s: #{msg} ."
-      ws.send "s: Hello , welcome to Line_J"
+      #ws.send "s: #{msg} ."
+      @channel.push @sid.to_s + ":#{msg}"
+      #ws.send "s: Hello , welcome to Line_J"
     }
   end
 }
