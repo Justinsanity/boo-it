3435fd0a3c27db11eece987ed5d4f7b20d06ff8c
Veck Hsiao <fbukevin@gmail.com>
2016-03-31 08:43:42 +0000

    WIP: implementing index.js and helper.js

--- tmp_express/routes/index.js
+++ tmp_express/routes/index.js
@@ -10,12 +10,64 @@ router.get('/', function(req, res){
 	res.render('hello');
 });
 
+/* action to direct to client to boo ! */
 router.get('/friend', function(req, res){
-	
+    if(session[id] == nil){
+        // session.clear
+        res.redirect('/');
+    } else {
+        // MUST to design a random key to hash the session
+        if(session[id] == mongo_id(session[id])){
+            // this part needs a algorithm to send ticket mechanism           
+            login_usr = username_by_id(session[id])
+            friends_json = friends_by_id(session[id])
+            res.render('friends_list', {
+            	locals: {
+            		user_name	: login_usr, 
+            		friends_list: friends_json
+            	}
+            });
+        } else {
+            /*
+             this part need to some soliutions
+            # 1: clean session(comparison)
+            # 2: query DB info. again
+            # 3: RANDOM KET SETTING
+            #"====#{@session_id}==="
+            #session[:id]
+            */
+            res.send("You are not permissioned to login!");
+        }
+    }
 });
 
+/* client for Boo-it chating! */
 router.post('/chat', function(req, res){
-	
+    if(session[id] == nil){
+        // session.clear
+        res.redirect('/');
+    } else {
+        if(session[id] == mongo_id(session[id])){
+            // this part needs a algorithm to send ticket mechanism           
+            login_usr = username_by_id(session[id])
+            // history send!
+            historyid = []
+            historyid.push(login_usr)
+            historyid.push(params[chat_f])
+            sort_his_id = historyid.sort
+            q_history_id = sort_his_id[0] + sort_his_id[1]
+            history_json = history_by_id(q_history_id)
+            res.render('chat', {
+            	locals: {
+            		user_name: login_usr,
+            		friend_name: params[chat_f],
+            		history_msg: history_json
+            	}
+            });
+        } else {
+            res.send("You are not permissioned to login!");
+        }
+    }
 });
 
 router.get('/redirect', function(req, res){
@@ -31,8 +83,8 @@ router.get('/logout', function(req, res){
 /* action to lookup database */
 router.get('/document/', function(req, res){
     // content_type :json
-    // settings.mongo_db['accounts'].find.to_a.to_json
-    // settings.mongo_db['dialog'].find.to_a.to_json
+    // settings.mongo_db['accounts'].find.to_json
+    // settings.mongo_db['dialog'].find.to_json
 });
 
 /* action to lookup by id */
@@ -47,11 +99,11 @@ router.post('/login', function(req, res){
 	var username = req.body.username;
 	var password = req.body.password;
 	
-    @username = params[:username]
-    @password = params[:password]
+    var username = params[username]
+    var password = params[password]
     
-    check_username(params[:username],params[:password])
-    #erb :result
+    check_username(params[username],params[password])
+    // erb :result
 });
 
 
